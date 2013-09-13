3721e238f3f5c8d2cae9ca80a3747add5dc3cffc
pramodkumar.email@gmail.com <pramodkumar.email@gmail.com>
2013-09-13 14:32:32 +0530

    v_005
    Create User through PHP

---
+++ php/Forms/register.php
@@ -0,0 +1,23 @@
+<?php
+$conn = mysql_connect("localhost","root","") or die('Connection Error...'.mysql_error());
+$db = mysql_select_db('sapient', $conn ) or die('DB Selection Error...'.mysql_error());
+
+$sql_query = "INSERT INTO info (fname, lname, email, pass) VALUES ('test','test','test','test')";
+$sql_query = "INSERT INTO info (fname, lname, email, pass) VALUES ('".$_POST['fName']."','".$_POST['lName']."','".$_POST['email']."','".$_POST['password']."')";
+$result = mysql_query($sql_query) or die('Error in the query...<br/>'. mysql_error());
+
+//header("Location: http://google.com");
+//echo "<script>window.location='http://google.com'</script>";
+//mysql_close($conn);
+?>
+
+
+<?php
+    $conn = mysql_connect("localhost","root","") or die('Connection Error...'.mysql_error());
+    $db = mysql_select_db('sapient', $conn ) or die('DB Selection Error...'.mysql_error());
+    $result = mysql_query($conn,"SELECT * FROM info");
+    $row = mysql_fetch_array($result)
+    echo $row["id"];
+    
+    mysql_close($conn);
+?>
