1272673af0b2ba12cf9b1002bb4fafdcd5e3f22e
pramodkumar.email@gmail.com <pramodkumar.email@gmail.com>
2013-10-08 09:49:24 +0530

    v_007
    formated data output

--- php/Forms/select_user.php
+++ php/Forms/select_user.php
@@ -1,8 +1,15 @@
 <?php
-
+    $conn = mysql_connect("localhost","root","") or die('Connection Error...'.mysql_error());
+    $db = mysql_select_db('sapient', $conn ) or die('DB Selection Error...'.mysql_error());
+    
     $result = mysql_query("SELECT * FROM info");
-    $row = mysql_fetch_array($result)
-    echo $row;
+    //$row = mysql_fetch_array($result);
+    echo '<pre>';
+    WHILE( $row = mysql_fetch_row($result)) {
+        echo "My ID is: " . $row['0'] . "<br/>";
+        echo "My name is: " . $row['1'] . ' '. $row['2'] . "<br/>";
+    }
+    echo '<pre>';
     
     //mysql_close($conn);
 ?>
